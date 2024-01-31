ec9c1e36742355516b2544ca8a4bc3617d64158d
DESKTOP-ALP9J2M\Usuario <csi23-mserina@colegioaltair.net>
2024-01-31 23:45:29 +0100

    no message

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace ejercicio2ObligatorioCompañero
+﻿using System.ComponentModel;
+
+namespace ejercicio2ObligatorioCompañero
 {
 
     class program
@@ -6,27 +8,89 @@
 
         static void Main(string[] args)
         {
+            bool cerrarBucle = false;
             int[,] tabla = new int[4,4];
+            int[,] tabla2 = new int[4,4];
+            Random random = new Random();
 
-            //Columnas
-            tabla[0, 1] = 1;
-            tabla[0, 2] = 2;
-            tabla[0, 3] = 3;
+            //array con los datos
+            for (int x = 0; x < tabla2.GetLength(0); x++)
+            {
+                for (int y = 0; y < tabla2.GetLength(1); y++)
+                {
+                    tabla2[x, y] = random.Next(0, 2);
+                }
+            }
 
-            //filas
-            
-            tabla[1,0] = 1;
-            tabla[2,0] = 2;
-            tabla[3,0] = 3;
-
+            //Para mostrar la tabla con los datos ocultos
             for (int x = 0; x < tabla.GetLength(0); x++)
             {
                 for (int y = 0; y < tabla.GetLength(1); y++)
                 {
+                    
+                    tabla[0, y] = y;
+                    tabla[x, 0] = x;
+                    if (tabla[0, 0] == 0 | tabla[0, 0] == 1)
+                    {
+                        tabla[0, 0] = -2;
+                    }
+
+                    if (tabla[x, y] == 0)
+                    {
+                        tabla[x, y] = tabla2[x,y];
+                    }
                     Console.Write(tabla[x, y] + "\t");
+
+                   
                 }
                 Console.WriteLine();
+            }
+            //Los intentos
+            Console.WriteLine(" ");
+            int contador = 0;
+            while(!cerrarBucle && contador <= 3)
+            {
+                Console.WriteLine(" ");
+                Console.WriteLine("Contador: " + contador);
+            //Para pedir al usuario la posicion X y Y
+                Console.WriteLine("Inserte posicion X");
+                int PosicionX = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Inserte posicion Y");
+                int PosicionY = Convert.ToInt32(Console.ReadLine());
+
+            //Para que imprima la tabla pero con las posiciones que nos inserto el usuario
+                for (int x = 0; x < tabla2.GetLength(0); x++)
+                {
+                    for (int y = 0; y < tabla2.GetLength(1); y++)
+                    {
+                        tabla[PosicionX, PosicionY] = tabla2[PosicionX, PosicionY];
+                        Console.Write(tabla[x, y] + "\t");
+                    }
+                    Console.WriteLine();
+                }
+                if (tabla[PosicionX, PosicionY] == 0)
+                {
+                    cerrarBucle = true;
+                    Console.WriteLine("Enorabuena");
+                }
+                else
+                {
+                    contador++;
+                    Console.WriteLine(" ");
+                    Console.WriteLine("Vuelve a intentarlo");
+                    
+                }
             }
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
